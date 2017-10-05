ffe8f4f0910d13f7a31520a6a388bcb87eb76229
Adam Pugh <adampugh@zoho.com>
2017-10-05 19:11:45 +0100

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -13,14 +13,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-
-store.dispatch(addExpense({description: "water bill", amount: 200}));
-store.dispatch(addExpense({description: "gas bill", amount: 9400}));
-store.dispatch(addExpense({description: "rent", amount: 5600, createdAt: 1000}));
-
-const state = store.getState();
-console.log(getVisibileExpsenses(state.expenses, state.filters));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
