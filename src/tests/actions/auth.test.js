c3bffcd696c96e9edf0fe66069719c81242685f1
Adam Pugh <adampugh@zoho.com>
2017-10-21 17:02:06 +0100

    Add private routes

--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -1,11 +1,11 @@
 import {login, logout} from "../../actions/auth";
 
-test("should gen login action object", () => {
+test("should generate login action object", () => {
     const uid = "abc";
     const action = login(uid);
     expect(action).toEqual({
         type: "LOGIN",
-        uid: uid
+        uid
     });
 });
 
--- src/tests/reducers/auth.test.js
+++ src/tests/reducers/auth.test.js
@@ -1,13 +1,13 @@
 import authReducer from "../../reducers/auth";
 
-test("should return a login id", () => {
+test("should set uid for login", () => {
     const state = authReducer(undefined, {type: "LOGIN", uid: "abc"});
     expect(state).toEqual({
         uid: "abc"
     });
 });
 
-test("should return empty object on logout", () => {
-    const state = authReducer(undefined, {type: "LOGOUT"});
+test("should clear uid for logout", () => {
+    const state = authReducer({uid: "abc"}, {type: "LOGOUT"});
     expect(state).toEqual({});
 });
